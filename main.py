1839cf3cd3eb726a01eed9dd7d6842d716c81325
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-02-08 14:51:16 +0300

    6-й коммит, import Command

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-from aiogram import Bot, Dispatcher, types, executor
+import asyncio
+from aiogram import Bot, Dispatcher, types
 
 from aiogram.types import WebAppInfo
+from aiogram.filters.command import Command
 
 from settings import TELEGRAM_TOKEN
 
 bot = Bot(TELEGRAM_TOKEN)
 
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
-@dp.message_handler(command=["start"])
+@dp.message(Command("start"))
 async def start(message: types.Message):
     """ Эта функция обрабатывает команду «/start» и отправляет приветственное сообщение с кнопкой для открытия веб-страницы """
     markup = types.ReplyKeyboardMarkup()
     markup.add(types.KeyboardButton("Открыть веб страницу", web_app=WebAppInfo(url = "https://ru.wikipedia.org/wiki/"),))
     await message.answer(f"Привет, {message.from_user.first_name} рад тебя видеть!👋", reply_markup=markup)
 
-executor.start_polling(dp)
+async def main():
+    """ Запуск процесса поллинга новых апдейтов """
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
