a89c6a945eeea1ba28ca0b7ad3a1fa5fc2194f49
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-02-08 14:09:02 +0300

    4-й коммит

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(command=["start"])
 async def start(message: types.Message):
+    """ Эта функция обрабатывает команду «/start» и отправляет приветственное сообщение с кнопкой для открытия веб-страницы """
     markup = types.ReplyKeyboardMarkup()
     markup.add(types.KeyboardButton("Открыть веб страницу", web_app=WebAppInfo(url = "https://ru.wikipedia.org/wiki/"),))
     await message.answer(f"Привет, {message.from_user.first_name} рад тебя видеть!👋", reply_markup=markup)
